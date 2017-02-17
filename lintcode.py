37e4ce8d320adc3518c9341beb100e8dd30a250e
caijingwei <ecaijw@msn.com>
2017-02-17 11:26:36 +0800

    prepare code

--- lintcode.py
+++ lintcode.py
@@ -2762,3 +2762,21 @@ class SolutionEditDistance:
     def minDistance(self, word1, word2):
         # write your code here
         pass
+
+# Longest Consecutive Sequence
+# Given an unsorted array of integers, find the length of
+# the longest consecutive elements sequence.
+# Clarification
+# Your algorithm should run in O(n) complexity.
+# Example
+# Given [100, 4, 200, 1, 3, 2],
+# The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+# TODO
+class SolutionLongestConsecutiveSequence:
+    """
+    @param num, a list of integer
+    @return an integer
+    """
+    def longestConsecutive(self, num):
+        # write your code here
+        pass
